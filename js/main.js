eb6162dc6ebf4b63d33355c494747f09888fd46c
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-10 16:33:56 -0500

    added javascript for radio and checkbox buttons

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
-(function () {
+
   const form = document.querySelector('form');
 
 //This function will create an array from the radio buttons and return which every one is checked
   function serializeRadio (form) {
-    const radios = Array.from(form.elements).filter(element => element.type === 'radio');
-
+    const radios = Array.from(form.elements).filter(element => element.type === 'radio'); // 'Array.from(form.elements)' method creates a new, shallow-copied Array instance from an array-like or iterable objects from the form
     const checkedRadio = radios.filter(radio => radio.checked === true);
-    return checkedRadio
+    const radioData = checkedRadio.map(radio => radio.value);
+    return radioData;
   }
 
 //This function will create an array from the radio buttons and return which every one is checked
   function serializeCheckbox (form) {
     const checkboxes = Array.from(form.elements).filter(element => element.type === 'checkbox');
-
-    const checkedBox = checkboxes.filter(checkbox => checkbox.checked === true);
-    return checkedBox
+    const checkedBoxes = checkboxes.filter(checkbox => checkbox.checked === true);
+    const checkedData = checkedBoxes.map(checkbox => checkbox.value);
+    return checkedData;
   }
 
-  console.log(serializeRadio(form));
-  console.log(serializeCheckbox(form));
-
-
-})();
-
-// 'Array.from(form.elements)' method creates a new, shallow-copied Array instance from an array-like or iterable objects from the form
+// This loop will place a two selection limit to the 'Sides' section
