f0f1f3f794646ef2c9f60b2b7ec1865c54ed2c3b
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-10 14:57:40 -0500

    added javascript to return checked items

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function () {
+  const form = document.querySelector('form');
+
+//This function will create an array from the radio buttons and return which every one is checked
+  function serializeRadio (form) {
+    const radios = Array.from(form.elements).filter(element => element.type === 'radio');
+
+    const checkedRadio = radios.filter(radio => radio.checked === true);
+    return checkedRadio
+  }
+
+//This function will create an array from the radio buttons and return which every one is checked
+  function serializeCheckbox (form) {
+    const checkboxes = Array.from(form.elements).filter(element => element.type === 'checkbox');
+
+    const checkedCheckbox = checkboxes.filter(checkbox => checkbox.checked === true);
+    return checkedRadio
+  }
+
+  console.log(serializeRadio(form));
+  console.log(serializeCheckbox(form));
+
+
+})();
+
+// 'Array.from(form.elements)' method creates a new, shallow-copied Array instance from an array-like or iterable objects from the form
