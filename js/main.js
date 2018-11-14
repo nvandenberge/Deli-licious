c1a4d219fbce02dbe38654986634c815dc6a3b94
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-14 17:55:59 -0500

    checkbox value display in output when checked

--- js/main.js
+++ js/main.js
@@ -11,30 +11,33 @@ const form = document.querySelector('form');
 
 //This function will create an array from the radio buttons and return which every one is checked
   function serializeCheckbox (form) {
+    const output = document.getElementById('output')
+    const li = document.createElement('li')
     const checkboxes = Array.from(form.elements).filter(element => element.type === 'checkbox');
     const checkedBoxes = checkboxes.filter(checkbox => checkbox.checked === true);
     const checkedData = checkedBoxes.map(checkbox => checkbox.value);
-    return checkedData;
+    output.innerText=checkedData;
   }
 
   //This function will update the order form when a value is selected/deselected
+  // const menuItem = document.getElementsByClassName('menuItem')
+  // const output = document.getElementById('output')
+  // const li = document.createElement('li')
+  //
+  // function addMenuItem(checked) {
+  //     for (let i = 0; i < menuItem.length; i++){
+  //       menuItem[i].onclick=function(){
+  //       if (menuItem.checked === true) {
+  //         return li.innerText = menuItem.value[i];
+  //
+  //       // li.createTextNode = menuItem.value;
+  //       // inputArea.append(li);
+  //       //   event.preventDefault();
+  //       }
+  //     }
+  //   }
+  // }
 
-  function addMenuItem() {
-    var menuItem = document.getElementsByClassName('menuItem');
-    var inputArea = document.querySelector('h2');
-    var li = document.createElement('li');
-      for (let i = 0; i < menuItem.length; i++){
-        menuItem[i].onclick=function(){
-        if (menuItem.checked === true) {
-          console.log(menuItem.value[i]);
-
-        // li.createTextNode = menuItem.value;
-        // inputArea.append(li);
-        //   event.preventDefault();
-        }
-      }
-    }
-  }
 
 // This loop will place a two selection limit to the 'Sides' section
 
