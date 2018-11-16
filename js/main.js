895950d4afaa2fee427f2c622975deb00e451dc1
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-16 17:21:27 -0500

    function for sides limit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 const form = document.querySelector('form');
 
 //This function will create an array from the radio buttons and return which every one is checked
@@ -11,53 +10,43 @@ const form = document.querySelector('form');
 
 //This function will create an array from the radio buttons and return which every one is checked
   function serializeCheckbox (form) {
-    const output = document.getElementById('output')
     const li = document.createElement('li')
     const checkboxes = Array.from(form.elements).filter(element => element.type === 'checkbox');
     const checkedBoxes = checkboxes.filter(checkbox => checkbox.checked === true);
     const checkedData = checkedBoxes.map(checkbox => checkbox.value);
-    output.innerText=checkedData;
+    document.getElementById('output').appendChild(li).innerHTML=checkedData;
   }
 
-  //This function will update the order form when a value is selected/deselected
-  // const menuItem = document.getElementsByClassName('menuItem')
-  // const output = document.getElementById('output')
-  // const li = document.createElement('li')
-  //
-  // function addMenuItem(checked) {
-  //     for (let i = 0; i < menuItem.length; i++){
-  //       menuItem[i].onclick=function(){
-  //       if (menuItem.checked === true) {
-  //         return li.innerText = menuItem.value[i];
-  //
-  //       // li.createTextNode = menuItem.value;
-  //       // inputArea.append(li);
-  //       //   event.preventDefault();
-  //       }
-  //     }
-  //   }
-  // }
 
+// This function will place a two selection max limit on the 'Sides' options
+// Why does the alert run twice and still adds value?
 
-// This loop will place a two selection limit to the 'Sides' section
+function maxTwo () {
+    document.querySelectorAll('.sideItem').forEach(element => element.addEventListener('change', maxTwo, false));
 
-function sidesLimit(sides, limit){
-	var sides = document.getElementsByName('sides');
-	var limit = 2
-	for (let i = 0; i < sides.length; i++){
-		sides[i].onclick=function(){
-		let checkedcount = 0
-		for (let i = 0; i < sides.length; i++)
-			checkedcount += (sides[i].checked)? 1 : 0
-		if (checkedcount > limit){
-			alert("You may only select a maximum of "+limit+" sides")
-			this.checked=false
-			}
-		}
-	}
+    if (document.querySelectorAll('.sideItem:checked').length > 2) {
+       alert('You may only select a maximum of 2 sides')
+        this.checked = false;
+    }
 }
 
 
-// var listItem = document.createElement('li');
-// var menuItem = document.getElementById('yourSandwich')
-// var input = document.querySelectorAll('input');
+
+
+
+        // Working function that uses a loop
+// function sidesLimit(sides, limit){
+// 	var sides = document.getElementsByName('sides');
+// 	var limit = 2
+// 	for (let i = 0; i < sides.length; i++){
+// 		sides[i].onclick=function(){
+// 		let checkedcount = 0
+// 		for (let i = 0; i < sides.length; i++)
+// 			checkedcount += (sides[i].checked)? 1 : 0
+// 		if (checkedcount > limit){
+// 			alert(`You may only select a maximum of ${limit} sides`)
+// 			this.checked=false
+// 			}
+// 		}
+// 	}
+// }
