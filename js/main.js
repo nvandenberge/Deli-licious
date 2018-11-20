cea83e7238363a4c1f4b680862bebb2868a5a300
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-20 15:41:56 -0500

    create li element with value from bread radio button

--- js/main.js
+++ js/main.js
@@ -1,67 +1,43 @@
 // This function will add radio button value
-function addRadio() {
-    const bread = document.getElementById('bread-select')
-    const li = document.createElement('li')
+  const bread = document.getElementById('bread-select')
+  const li = document.createElement('li') //Used to create <li> element
+  const breadItem = document.querySelectorAll('.breadItem:checked')
+  const form = document.getElementById('menu')
 
-if (this.checked) {
-      li.innerHTML = this.value
-      bread.appendChild(li)
-    } else if (this.checked === false) {
-        if (this.value === li.innerHTML) {
-          bread.removeChild(li);
-        }
-      }
+  function addRadio() {
+    const radios = Array.from(form.elements).filter(element => element.type === 'radio'); // 'Array.from(form.elements)' method creates a new, shallow-copied Array instance from an array-like or iterable objects from the form element
+    const checkedRadio = radios.filter(radio => radio.checked === true); //filter for selected radio button
+    var radioData = checkedRadio.map(radio => radio.value); //creates an array with value from selected radio 
+    // console.log(radioData);
+
+    if (breadItem.length = 1) {
+      li.innerText = radioData;
+      bread.appendChild(li);
     }
+  }
 
 document.querySelectorAll('[type="radio"]').forEach(element => element.addEventListener('change', addRadio, false));
 
-
 // This function will add selected checkbox value, remove them, and place 2 max limit on Sides
 function addCheckbox() {
-    const ul = document.getElementById('output')
-    const sideItems = document.querySelectorAll('.sideItem:checked')
-    const li = document.createElement('li')
+  const ul = document.getElementById('output')
+  const sideItems = document.querySelectorAll('.sideItem:checked')
+  const li = document.createElement('li')
 
-    if (sideItems.length > 2) {
-      alert('You may only select a maximum of 2 sides')
-      this.checked = false;
-    } else if (this.checked === true) {
-      li.innerHTML = this.value
-      ul.appendChild(li)
-    } else if (this.checked === false) {
-      const children = Array.from(ul.children);
-      children.forEach(function(child) {
-        if (this.value === child.innerText) {
-          ul.removeChild(child);
-        }
-      }.bind(this))
-    }
+  if (sideItems.length > 2) {
+    alert('You may only select a maximum of 2 sides')
+    this.checked = false;
+  } else if (this.checked === true) {
+    li.innerHTML = this.value
+    ul.appendChild(li)
+  } else if (this.checked === false) {
+    const children = Array.from(ul.children);
+    children.forEach(function(child) {
+      if (this.value === child.innerText) {
+        ul.removeChild(child);
+      }
+    }.bind(this))
+  }
 }
 
 document.querySelectorAll('[type="checkbox"]').forEach(element => element.addEventListener('change', addCheckbox, false));
-
-
-
-
-// const form = document.querySelector('form');
-// const ul = document.getElementById('output')
-// const menuItem = document.querySelectorAll('[type="checkbox"]:checked')
-//
-// function addCheckbox() {
-//     if ((this.name === 'condiments' || 'toppings') && (menuItem.length > 2)) {
-//       alert('You may only select a maximum of 2 sides')
-//       this.checked = false;
-//     } else if (this.checked === true) {
-//       const li = document.createElement('li')
-//       li.innerHTML = this.value
-//       ul.appendChild(li)
-//     } else if (this.checked === false) {
-//       const children = Array.from(ul.children);
-//       children.forEach(function(child) {
-//         if (this.value === child.innerText) {
-//           ul.removeChild(child);
-//         }
-//       }.bind(this))
-//     }
-// }
-// document.querySelectorAll('[type="checkbox"]').forEach(element => element.addEventListener('change', addCheckbox, false));
--- js/old.js
+++ js/old.js
@@ -32,3 +32,27 @@
 // 		}
 // 	}
 // }
+
+
+// const form = document.querySelector('form');
+// const ul = document.getElementById('output')
+// const menuItem = document.querySelectorAll('[type="checkbox"]:checked')
+//
+// function addCheckbox() {
+//     if ((this.name === 'condiments' || 'toppings') && (menuItem.length > 2)) {
+//       alert('You may only select a maximum of 2 sides')
+//       this.checked = false;
+//     } else if (this.checked === true) {
+//       const li = document.createElement('li')
+//       li.innerHTML = this.value
+//       ul.appendChild(li)
+//     } else if (this.checked === false) {
+//       const children = Array.from(ul.children);
+//       children.forEach(function(child) {
+//         if (this.value === child.innerText) {
+//           ul.removeChild(child);
+//         }
+//       }.bind(this))
+//     }
+// }
+// document.querySelectorAll('[type="checkbox"]').forEach(element => element.addEventListener('change', addCheckbox, false));
