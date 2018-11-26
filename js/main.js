7bec04d0f5f8178556a85f2085b221a7e737857d
Nick Vandenberge <nvandenberge14@gmail.com>
2018-11-26 14:58:01 -0500

    added functions to add checkbox selection to list // only displays one of the checked boxes at the moment, not all as needed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-// This function will add radio button value
-const menuItem = document.querySelectorAll('.menuItem:checked')
-const sandwichOutput = document.getElementById('yourSandwich')
 const form = document.getElementById('menu')
+const radioButtons = document.getElementById('radioButtons')
+const checkboxButtons = document.getElementById('checkboxButtons')
 
 function renderRadio (data) {
 let { bread, cheese, meats } = data;
 
-  return `<ul id='output'>
+  return `<ul id='radioOutput'>
     <h2 id ='bread-select'>Bread</h2>
     <li>${bread ? bread : ''}</li>
     <h2 id ='cheese-select'>Cheese</h2>
@@ -23,50 +22,32 @@ function addRadio() {
   checkedRadio.forEach(radio => {
     return radioData[radio.name] = radio.value
   })
-  sandwichOutput.innerHTML = renderRadio(radioData);
+  radioButtons.innerHTML = renderRadio(radioData);
 }
 document.querySelectorAll('[type="radio"]').forEach(element => element.addEventListener('change', addRadio, false));
 
 
-// This function will add selected checkbox value, remove them, and place 2 max limit on Sides
-function addCheckbox() {
-  const ul = document.getElementById('output')
-  const sideItems = document.querySelectorAll('.sideItem:checked')
-  const li = document.createElement('li')
+function renderCheckbox (data) {
+let { toppings, condiments, sides } = data;
 
-  if (sideItems.length > 2) {
-    alert('You may only select a maximum of 2 sides')
-    this.checked = false;
-  } else if (this.checked === true) {
-    li.innerHTML = this.value
-    ul.appendChild(li)
-  } else if (this.checked === false) {
-    const children = Array.from(ul.children);
-    children.forEach(function(child) {
-      if (this.value === child.innerText) {
-        ul.removeChild(child);
-      }
-    }.bind(this))
-  }
+  return `<ul id='checkboxOutput'>
+    <h2 id ='toppings-select'>Toppings</h2>
+    <li>${toppings ? toppings : ''}</li>
+    <h2 id ='condiments-select'>Condiments</h2>
+    <li>${condiments ? condiments : ''}</li>
+    <h2 id ='sides-select'>Sides</h2>
+    <li>${sides ? sides : ''}</li>
+  </ul>`
 }
 
+function addCheckbox() {
+  const checkboxes = Array.from(form.elements).filter(element => element.type === 'checkbox');
+  const checkedBoxes = checkboxes.filter(checkbox => checkbox.checked === true);
+  let checkboxData = {};
+  checkedBoxes.forEach(checkbox => {
+    // var checkedData = checkedBoxes.map(checkbox => checkbox.value);
+    return checkboxData[checkbox.name] = checkbox.value
+  })
+  checkboxButtons.innerHTML = renderCheckbox(checkboxData);
+}
 document.querySelectorAll('[type="checkbox"]').forEach(element => element.addEventListener('change', addCheckbox, false));
-
-
-
-// const bread = document.getElementById('bread-select')
-// const li = document.createElement('li') //Used to create <li> element
-// const breadItem = document.querySelectorAll('.breadItem:checked')
-// const form = document.getElementById('menu')
-//
-// function addRadio() {
-//   const radios = Array.from(form.elements).filter(element => element.type === 'radio'); // 'Array.from(form.elements)' method creates a new, shallow-copied Array instance from an array-like or iterable objects from the form element
-//   const checkedRadio = radios.filter(radio => radio.checked === true); //filter for selected radio button
-//   var radioData = checkedRadio.map(radio => radio.value); //creates an array with value from selected radio
-//   // console.log(radioData);
-//
-//   if (breadItem.length) {
-//     li.innerText = radioData;
-//     bread.appendChild(li); //creates new <li> element under bread header
-//   }
-// }
