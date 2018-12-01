b30a5623984196b8b97e45f88d1390fad4abcf25
Nick Vandenberge <nvandenberge14@gmail.com>
2018-12-01 12:53:05 -0500

    styling to homepage // added confirm.html and README // restructed main.js functions to create objects from selected inputs

---
+++ README.md
@@ -0,0 +1,17 @@
+# Deli-*licious*
+
+ - Deli-*licious* is a sandwich building web app powered by HTML, CSS, and Vanilla Javascript. In addition, this is my first web app built as an aspiring web developer and after attending a 10-week Front End Web Developer (FEWD) course provided by Betamore Academy in Baltimore, MD.
+
+# Purpose
+ - Create your own sandwich using the radio and checkbox buttons while being to view your full order in the right hand output box as you select your items.
+
+# Goal
+ - My goal of Deli-*licious* was to pratice and expand knowledge/skills on web development.
+
+# Javascript Concepts Involved
+  - Functions
+  - Arrays
+  - Objects
+  - DOM Manipulation
+  - Event Listeners
+  - Form Validation
--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const form = document.getElementById('menu')
 const radioButtons = document.getElementById('radioButtons')
 const checkboxButtons = document.getElementById('checkboxButtons')
+const sideItems = document.querySelectorAll('.sideItem:checked')
 
-function renderRadio (data) {
-let { bread, cheese, meats } = data;
+function renderRadio(data) {
+  let {
+    bread,
+    cheese,
+    meats
+  } = data; //Creates objects with the selected radio value
 
   return `<ul id='radioOutput'>
     <h2 id ='bread-select'>Bread</h2>
@@ -13,7 +18,8 @@ let { bread, cheese, meats } = data;
     <h2 id ='meat-select'>Meat</h2>
     <li>${meats ? meats : ''}</li>
   </ul>`
-}
+} //Generatres headers and list items for selected radio buttons
+  //If true, display value, if false, display blank
 
 function addRadio() {
   const radios = Array.from(form.elements).filter(element => element.type === 'radio'); //'Array.from(form.elements)' method creates a new, shallow-copied Array instance from an array-like or iterable objects from the form element
@@ -24,30 +30,52 @@ function addRadio() {
   })
   radioButtons.innerHTML = renderRadio(radioData);
 }
+
 document.querySelectorAll('[type="radio"]').forEach(element => element.addEventListener('change', addRadio, false));
 
 
-function renderCheckbox (data) {
-let { toppings, condiments, sides } = data;
+function renderCheckbox ({ toppings = [], condiments = [], sides = [] }) {
 
-  return `<ul id='checkboxOutput'>
-    <h2 id ='toppings-select'>Toppings</h2>
-    <li>${toppings ? toppings : ''}</li>
-    <h2 id ='condiments-select'>Condiments</h2>
-    <li>${condiments ? condiments : ''}</li>
-    <h2 id ='sides-select'>Sides</h2>
-    <li>${sides ? sides : ''}</li>
-  </ul>`
-}
+  // if (sides.length === 2) {
+  //     sides.checked
+  //   }
+  //   sides.checked=false;
+  // }
+  // else {
+  return `
+      <ul id='checkboxOutput'>
+      <h2 id ='toppings-select'>Toppings</h2>
+      ${toppings.map(topping => `<li>${topping}</li>`).join('')}
+      <h2 id ='condiments-select'>Condiments</h2>
+      ${condiments.map(condiment => `<li>${condiment}</li>`).join('')}
+      <h2 id ='sides-select'>Sides(2)</h2>
+      ${sides.map(side => `<li>${side}</li>`).join('')}
+    </ul>`;
+  }
+
 
 function addCheckbox() {
   const checkboxes = Array.from(form.elements).filter(element => element.type === 'checkbox');
   const checkedBoxes = checkboxes.filter(checkbox => checkbox.checked === true);
-  let checkboxData = {};
-  checkedBoxes.forEach(checkbox => {
-    // var checkedData = checkedBoxes.map(checkbox => checkbox.value);
-    return checkboxData[checkbox.name] = checkbox.value
-  })
+  const checkboxData = checkedBoxes.reduce((obj, checkbox) => {
+  obj[checkbox.name] = [...(obj[checkbox.name]||[]), checkbox.value];
+  return obj;
+}, {});
   checkboxButtons.innerHTML = renderCheckbox(checkboxData);
 }
+
 document.querySelectorAll('[type="checkbox"]').forEach(element => element.addEventListener('change', addCheckbox, false));
+
+const breadItems = document.querySelectorAll('.breadItem:checked')
+function validateForm () {
+  if (breadItems.length === 0) {
+    alert('Please select your bread.')
+    breadItem.focus();
+    return false;
+ }
+ else {
+
+ }
+}
+
+document.querySelectorAll('[type="submit"]').forEach(element => element.addEventListener('click', validateForm, false));
